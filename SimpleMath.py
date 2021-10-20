bc5581b11fd77397e8f100b43f9e39f97d546d40
Prasanga Fernando <prasangafdz@outlook.com>
2021-10-20 15:08:47 +0530

    Update SimpleMath.py

--- SimpleMath.py
+++ SimpleMath.py
@@ -3,3 +3,22 @@ Created on Oct 20, 2021
 
 @author: Prasanga Fernando
 '''
+print(3+4)
+
+print(10-8)
+
+print(3*4)
+
+print(12/3)
+
+print(12//3) #To prevent getting float value in output, we need to use double forward slashes
+
+print(19%8)
+
+print(580.2345/2.23532)
+
+print(2*2*2)
+
+print(2**5) #Using ** we can directly get the exponent without repeating number of times 
+
+print(3*4+5-6//2)
