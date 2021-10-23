b84d6afc84f011aa71f8195e4fa69ed66cc72672
Prasanga Fernando <prasangafdz@outlook.com>
2021-10-23 16:40:05 +0530

    Used some buit in functions to get math outputs
    Also used help() function to identify the built in functions

--- BuiltInFunctions.py
+++ BuiltInFunctions.py
@@ -3,3 +3,24 @@ Created on Oct 23, 2021
 
 @author: Prasanga Fernando
 '''
+
+
+#print(help())
+
+print("..."+"built-ins...")
+print(dir(__builtins__))
+
+num = 3
+
+print(pow(num, 2))
+
+print(max(12, 61,3,59,90,231))
+
+print(min(12, 61,3,59,90,231))
+
+print(abs(-23212))
+
+lengthOfString = len("TestString") #This returns the length of a string
+print(lengthOfString)
+
+print(help(pow))
