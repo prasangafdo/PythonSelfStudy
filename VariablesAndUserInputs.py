5cc905104c115b17a58f3c13b7fb7c10000dbb2f
Prasanga Fernando <prasangafdz@outlook.com>
2021-10-23 08:19:54 +0530

    Create VariablesAndUserInputs.py
    Captured user input and printed the user entered value

---
+++ VariablesAndUserInputs.py
@@ -0,0 +1,15 @@
+'''
+Created on Oct 20, 2021
+
+@author: Prasanga Fernando
+'''
+
+myVariable = 20 #We don't need to include data type when declaring a variable
+
+print(myVariable)
+
+print(myVariable+30)#This will add 30 to current value of the given variable
+
+userInput = input("Enter a value...")#This will capture the user input but that will be captured in String type
+
+print("The value you entered is "+userInput)
