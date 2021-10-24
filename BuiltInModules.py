03116eba79170f4377e55f62a681ab0ef874251e
Prasanga Fernando <prasangafdz@outlook.com>
2021-10-24 12:40:53 +0530

    Updated BuiltInModules.py

--- BuiltInModules.py
+++ BuiltInModules.py
@@ -3,3 +3,10 @@ Created on Oct 23, 2021
 
 @author: Prasanga Fernando
 '''
+import math
+
+print(int(math.sqrt(9)))
+
+squreRoot = math.sqrt #Here we do not need to include parentheses like we use in Java
+
+print(int(squreRoot(16)))
