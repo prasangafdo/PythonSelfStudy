4b437342563da4ae775bf0967cd585af0cdab9d4
Prasanga Fernando <prasangafdz@outlook.com>
2021-10-24 12:52:25 +0530

    Created SimpleCalculator module

---
+++ SimpleCalculator.py
@@ -0,0 +1,13 @@
+'''
+Created on Oct 24, 2021
+
+@author: Prasanga Fernando
+'''
+print("This is a simple calculator created by Prasanga Fernando using Python \nOnly performs addition...")
+
+x = int(input("Enter the first number"))
+y = int(input("Enter the second number"))
+
+print(x+y)
+
+
