007f9b5038ef9eaa19c75504b7bcf2ea8dd45cab
Prasanga Fernando <prasangafdz@outlook.com>
2021-10-28 16:07:57 +0530

    Update IfElse.py

--- IfElse.py
+++ IfElse.py
@@ -5,3 +5,15 @@ Created on Oct 28, 2021
 '''
 
 print("This is a small program to use conditional statements in Python")
+
+a = 100
+b = 50
+
+
+
+if(a>b):
+    maxValue = a
+else:
+    maxValue = b
+    
+print(maxValue)
