9ed1e13652d8366e4f491a848261904596526cc6
Prasanga Fernando <prasangafdz@outlook.com>
2021-10-29 12:47:38 +0530

    Update PythonStrings.py

---
+++ PythonStrings.py
@@ -0,0 +1,13 @@
+'''
+Created on Oct 29, 2021
+
+@author: Prasanga Fernando
+'''
+
+x = 'This is a string in single quotes'
+y = "This is a string in double quotes"
+z = """This is a string in tripple quotes"""
+
+print(x)
+print(y)
+print(z)
