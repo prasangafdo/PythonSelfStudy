89df11ecb181135179f45a1a4080252659c7edba
Prasanga Fernando <prasangafdz@outlook.com>
2021-10-30 16:57:10 +0530

    Update SimpleCalculator.py

--- SimpleCalculator.py
+++ SimpleCalculator.py
@@ -3,11 +3,15 @@ Created on Oct 24, 2021
 
 @author: Prasanga Fernando
 '''
-print("This is a simple calculator created by Prasanga Fernando using Python \nOnly performs addition...")
+print("This is a simple calculator created by Prasanga Fernando using Python \nOnly performs addition and deletion...")
 
 x = int(input("Enter the first number"))
 y = int(input("Enter the second number"))
+operation = input("Enter '1' for addition or '2' for subtraction")
 
-print(x+y)
+if operation == "1":
+    print("Answer is :"+str(x+y))
+else:
+    print("Answer is :"+str(x-y))
 
 input("Press enter to exit") #If we don't use this, it will terminate the application immediately
