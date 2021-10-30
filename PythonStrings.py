772e788f870ed1d02857008a65839e95c227b21b
Prasanga Fernando <prasangafdz@outlook.com>
2021-10-30 08:26:14 +0530

    Update PythonStrings.py

--- PythonStrings.py
+++ PythonStrings.py
@@ -4,6 +4,7 @@ Created on Oct 29, 2021
 @author: Prasanga Fernando
 '''
 
+#We can use any of these ways to initiate strings in Python
 x = 'This is a string in single quotes'
 y = "This is a string in double quotes"
 z = """This is a string in tripple quotes"""
@@ -11,3 +12,22 @@ z = """This is a string in tripple quotes"""
 print(x)
 print(y)
 print(z)
+
+escapeSequence = 'In python, escape sequence is \'backslash\''
+escapeSequence2 = "In python, escape sequence is \"backslash\""
+printBackslash = "Print this backslash \\ \nThis is a new line"
+
+print(escapeSequence)
+print(escapeSequence2)
+print(printBackslash)
+
+#Printing the same result multiple times
+sampleString = "Hi "
+
+print(sampleString * 10)
+
+#Type casting int -> String
+
+number1 = 40
+
+print(sampleString + str(number1))
