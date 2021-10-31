51a4999711901fa03f431828f72200a527ce5b6b
Prasanga Fernando <prasangafdz@outlook.com>
2021-10-31 17:58:31 +0530

    Update PythonList.py

--- PythonList.py
+++ PythonList.py
@@ -3,3 +3,11 @@ Created on Oct 31, 2021
 
 @author: Prasanga Fernando
 '''
+names=["Supun", "Kasun", "Gihan"]
+print(names)
+
+#In Python we can have index backwards also. It starts from -1
+print(names[-2])#This will print second value from last
+
+names.append("New name") #This will add a new value
+print(names[-1])
