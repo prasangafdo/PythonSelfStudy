8c3a3c8b24076f9577155f9a2e552a8a1ad7fd8c
Prasanga Fernando <prasangafdz@outlook.com>
2021-10-31 17:06:24 +0530

    Added nested if else

--- IfElse.py
+++ IfElse.py
@@ -9,11 +9,25 @@ print("This is a small program to use conditional statements in Python")
 a = 100
 b = 50
 
-
-
 if(a>b):
     maxValue = a
 else:
     maxValue = b
     
 print(maxValue)
+
+category = input("Enter category")
+
+
+if category== "pet":
+    animalName = input("Enter pet type")
+    if animalName == "dog":
+        print("You are a "+animalName+" person")
+    elif animalName == "cat":
+        print("You are a "+animalName+" person")
+    else:
+        print("You are missing a great time without a pet")
+elif category== "car":
+    print("You're a car person")
+       
+#Nested if statements
--- SimpleCalculator.py
+++ SimpleCalculator.py
@@ -3,7 +3,7 @@ Created on Oct 24, 2021
 
 @author: Prasanga Fernando
 '''
-print("This is a simple calculator created by Prasanga Fernando using Python \nOnly performs addition and deletion...")
+print("This is a simple calculator created by Prasanga Fernando using Python \nOnly performs addition, deletion and multiplication...")
 
 x = int(input("Enter the first number"))
 y = int(input("Enter the second number"))
@@ -13,6 +13,9 @@ if operation == "1":
     print("Answer is :"+str(x+y))
 elif operation == "2":
     print("Answer is :"+str(x-y))
+elif operation =="3":
+    print("Answer is: "+str(x*y))    
 else:
     print("Incorrect input")
 input("Press enter to exit") #If we don't use this, it will terminate the application immediately
+
