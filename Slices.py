c87cd5802e23e8c5fa215dc489a63168e0552e37
Prasanga Fernando <prasangafdz@outlook.com>
2021-11-06 17:37:29 +0530

    Update Slices.py

--- Slices.py
+++ Slices.py
@@ -5,4 +5,26 @@ Created on Nov 5, 2021
 '''
 myList = [2,4,54,23,67,34,987,12]
 
-print(myList[4:987])#This will include the first value and excludes the second value
+print(myList[4:34])#This will include the first value and excludes the second value
+
+print(myList[2:7])#We can either put values or indexes
+
+
+newList = myList[1:5]
+print(newList)
+
+#Printing including the last value of the list
+print(myList[2:8])
+
+print(myList[4:])
+
+print(myList[:5])
+
+#Using negative indexes
+print(myList[-6:-2]) #Printing starts from left side, so the left side's absolute value should be higher
+
+print(myList[-1:])
+
+print(myList[:-4])
+
+print(myList , myList[:])#Both of these return the same value
