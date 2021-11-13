c9d335117673ba2bb9f968d64deb46f575df564c
Prasanga Fernando <prasangafdz@outlook.com>
2021-11-13 17:12:17 +0530

    Update Comparison.py

--- Comparison.py
+++ Comparison.py
@@ -17,3 +17,22 @@ Logical operators
 and
 or
 '''
+
+print(1<3)
+
+print(3<=3)
+
+print(5>3)
+
+print(3>=2)
+
+print(5!=3)
+
+print(3<=3) and (5!=4)
+
+print(3<=3) or (32<2)
+
+#in
+testString = "abcdefg"
+
+print('a' in testString)
