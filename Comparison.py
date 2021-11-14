baf7977b0555617f6fbcffeccd7bc3c66ef55e3d
Prasanga Fernando <prasangafdz@outlook.com>
2021-11-14 10:27:14 +0530

    Update Comparison.py

--- Comparison.py
+++ Comparison.py
@@ -36,3 +36,17 @@ print(3<=3) or (32<2)
 testString = "abcdefg"
 
 print('a' in testString)
+
+print('ab' in testString)
+
+print('ac' in testString)
+
+a=[1,2,3]
+
+b=[1,2,3]
+
+print(a is b)#This will return false. Because these are not logically equal
+
+c=d=[1,2,3]
+
+print(c is d)#This will return true
