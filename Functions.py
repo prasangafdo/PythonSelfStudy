3af7d80e609609762120f01d246c58fda2e519e9
Prasanga Fernando <prasangafdz@outlook.com>
2021-11-28 13:55:38 +0530

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -3,3 +3,20 @@ Created on Nov 27, 2021
 
 @author: Prasanga Fernando
 '''
+def printName(name):
+    print(name)
+    
+def welcomeNote():
+    print("Hello.. welcome to test run")
+    
+welcomeNote()
+printName("Prasanga")
+printName("Supun")
+printName("Kaveesh")
+
+#Function with return values
+def addTwoNumbers(x,y):
+    return (x+y)
+
+sum = addTwoNumbers(10,21)
+print(sum)
