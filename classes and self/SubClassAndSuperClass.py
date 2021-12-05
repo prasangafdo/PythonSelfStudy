b2dde5ef840559910d9a8a26c47fdfd2b2a74402
Prasanga Fernando <prasangafdz@outlook.com>
2021-12-05 15:34:04 +0530

    Multiple inheritance

--- classes and self/SubClassAndSuperClass.py
+++ classes and self/SubClassAndSuperClass.py
@@ -7,9 +7,13 @@ class Parent:
     number1 = 43
     number2 = 32
     
-class Child (Parent):
+class Parent2:
+    stringTest = "Hello, I'm a string in Parent2 class"
+    
+class Child (Parent, Parent2): #Multiple inheritance
     pass #We need to use pass to fix the indentation issue 
 
 childObject = Child()
 print(childObject.number1)
 print(childObject.number2)
+print(childObject.stringTest)
