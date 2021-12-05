36aae0f52e4a54f11f104980239f30b758afd21b
Prasanga Fernando <prasangafdz@outlook.com>
2021-12-05 19:12:39 +0530

    Update ATMExample.py

--- Practice Codes/ATMExample.py
+++ Practice Codes/ATMExample.py
@@ -6,7 +6,18 @@ Created on Dec 5, 2021
 
 #Scenario -> Withdraw money from ATM when the account balance is 50k
 
-class BackgroundWorker(self):
-    
+class BackgroundWorker:
+    accountBalance = 50000
+    def withdrawMoney(self,requestedAmount):
+        print("Current account balance is ", self.accountBalance)
+        if(requestedAmount<= self.accountBalance):
+                print("Transaction completed! \nCurrent account balance is ", self.accountBalance - requestedAmount)      
+        else:
+            print("Account balance is not sufficient")
+            
+#class Runner:
 
-class Runner(BackgroundWorker):
+worker = BackgroundWorker()
+#while worker.accountBalance !=0:
+amount = int(input("Please enter the amount you want to withdraw"))
+worker.withdrawMoney(amount)
