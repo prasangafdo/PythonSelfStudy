69d8ae74527d4ed712845237552b67ea865cdc7e
Prasanga Fernando <prasangafdz@outlook.com>
2021-12-05 18:29:45 +0530

    Create ATMExample.py

---
+++ Practice Codes/ATMExample.py
@@ -0,0 +1,12 @@
+'''
+Created on Dec 5, 2021
+
+@author: Prasanga Fernando
+'''
+
+#Scenario -> Withdraw money from ATM when the account balance is 50k
+
+class BackgroundWorker(self):
+    
+
+class Runner(BackgroundWorker):
