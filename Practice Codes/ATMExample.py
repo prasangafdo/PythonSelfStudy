69436bc0eeb908861257449973650fa9c02f5bcc
Prasanga Fernando <prasangafdz@outlook.com>
2021-12-06 13:07:04 +0530

    Fixed balance not reducing issue
    Added a loop to continuously run the program till the account balance reaches 0

--- Practice Codes/ATMExample.py
+++ Practice Codes/ATMExample.py
@@ -7,17 +7,24 @@ Created on Dec 5, 2021
 #Scenario -> Withdraw money from ATM when the account balance is 50k
 
 class BackgroundWorker:
-    accountBalance = 50000
-    def withdrawMoney(self,requestedAmount):
-        print("Current account balance is ", self.accountBalance)
-        if(requestedAmount<= self.accountBalance):
-                print("Transaction completed! \nCurrent account balance is ", self.accountBalance - requestedAmount)      
-        else:
-            print("Account balance is not sufficient")
-            
+  accountBalance = 50000
+  def withdrawMoney(self):
+      
+       while self.accountBalance !=0: 
+        requestedAmount = int(input("\nEnter amount to withdraw or enter 0 to exit"))
+      #  print("Current account balance is ", self.accountBalance)
+        if requestedAmount!=0:
+            if(requestedAmount<= self.accountBalance):
+                    print("Transaction completed! \nCurrent account balance is ", self.accountBalance - requestedAmount)     
+                    self.accountBalance = self.accountBalance - requestedAmount 
+            else:
+                print("Account balance is not sufficient")
+        elif requestedAmount==0:
+            print("Thank you for banking with us")
+            quit()    
 #class Runner:
 
 worker = BackgroundWorker()
-#while worker.accountBalance !=0:
-amount = int(input("Please enter the amount you want to withdraw"))
-worker.withdrawMoney(amount)
+print("Your current balance is", worker.accountBalance)
+#amount = int(input("\nEnter amount to withdraw or enter 0 to exit"))
+worker.withdrawMoney()
