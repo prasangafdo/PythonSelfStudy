9e6913aaca9908c83c61ad3c43796dae6224be26
Prasanga Fernando <prasangafdz@outlook.com>
2021-12-11 12:23:59 +0530

    Update ElectricityBillCalculatorExample.py
    Added class variables and constructor to calculate bill amount

--- Practice Codes/ElectricityBillCalculatorExample.py
+++ Practice Codes/ElectricityBillCalculatorExample.py
@@ -3,3 +3,25 @@ Created on Dec 8, 2021
 
 @author: Prasanga Fernando
 """
+
+
+class ElectricityBillCalculator:
+    billAmount = None
+
+    def __init__(self, pointsCount):
+        if 50 < pointsCount:
+            self.billAmount = pointsCount * 5
+        elif 50 > pointsCount > 20:
+            self.billAmount = pointsCount * 3
+        else:
+            self.billAmount = pointsCount * 2
+
+
+bill1 = ElectricityBillCalculator(20)
+print(bill1.billAmount)
+bill1 = ElectricityBillCalculator(19)
+print(bill1.billAmount)
+bill1 = ElectricityBillCalculator(40)
+print(bill1.billAmount)
+bill1 = ElectricityBillCalculator(70)
+print(bill1.billAmount)
