ce6a2081fe7f9bb8bf592eeb1f3ee957572f6134
Prasanga Fernando <prasangafdz@outlook.com>
2021-12-11 12:58:09 +0530

    Update ElectricityBillCalculatorExample.py

--- Practice Codes/ElectricityBillCalculatorExample.py
+++ Practice Codes/ElectricityBillCalculatorExample.py
@@ -9,19 +9,33 @@ class ElectricityBillCalculator:
     billAmount = None
 
     def __init__(self, pointsCount):
-        if 50 < pointsCount:
-            self.billAmount = pointsCount * 5
-        elif 50 > pointsCount > 20:
-            self.billAmount = pointsCount * 3
-        else:
+        if pointsCount < 20:
             self.billAmount = pointsCount * 2
+        elif 19 < pointsCount < 50:
+            higherAmount = pointsCount - 20
+            calculatedAmount = higherAmount * 3 + (pointsCount - higherAmount) * 2
+            self.billAmount = calculatedAmount
+            # higherAmount = pointsCount - 50
+            # calculatedAmount = higherAmount*5
+            # lowerAmount = pointsCount - higherAmount
+        else:
+            print("Highh")
 
+        # if 50 < pointsCount:
+        #     self.billAmount = pointsCount * 5
+        # elif 50 > pointsCount > 20:
+        #     self.billAmount = pointsCount * 3
+        # else:
+        #     self.billAmount = pointsCount * 2
 
-bill1 = ElectricityBillCalculator(20)
+bill1 = ElectricityBillCalculator(21)
 print(bill1.billAmount)
-bill1 = ElectricityBillCalculator(19)
-print(bill1.billAmount)
-bill1 = ElectricityBillCalculator(40)
-print(bill1.billAmount)
-bill1 = ElectricityBillCalculator(70)
-print(bill1.billAmount)
+
+# bill1 = ElectricityBillCalculator(20)
+# print(bill1.billAmount)
+# bill1 = ElectricityBillCalculator(19)
+# print(bill1.billAmount)
+# bill1 = ElectricityBillCalculator(40)
+# print(bill1.billAmount)
+# bill1 = ElectricityBillCalculator(70)
+# print(bill1.billAmount)
