f83b00fbb42f8304d149355a919e460441fa172b
Prasanga Fernando <prasangafdz@outlook.com>
2021-12-12 12:18:39 +0530

    Updated the code to count the bill from the user input

--- Practice Codes/ElectricityBillCalculatorExample.py
+++ Practice Codes/ElectricityBillCalculatorExample.py
@@ -8,20 +8,20 @@ Created on Dec 8, 2021
 class ElectricityBillCalculator:
     billAmount = None
 
-    def __init__(self, pointsCount):
-        if pointsCount < 20:
-            self.billAmount = pointsCount * 2
-        elif 19 < pointsCount < 50:
-            higherAmount = pointsCount - 20
-            calculatedAmount = higherAmount * 3 + (pointsCount - higherAmount) * 2
+    def __init__(self, unitsCount):
+        if unitsCount < 20:
+            self.billAmount = unitsCount * 2
+        elif 19 < unitsCount < 50:
+            higherAmount = unitsCount - 20
+            calculatedAmount = higherAmount * 3 + (unitsCount - higherAmount) * 2
             self.billAmount = calculatedAmount
             # higherAmount = pointsCount - 50
             # calculatedAmount = higherAmount*5
             # lowerAmount = pointsCount - higherAmount
-        elif pointsCount > 49:
-            highestAmount = pointsCount - 50
-            higherAmount = pointsCount - highestAmount - 20
-            calculatedAmount = higherAmount * 3 + highestAmount * 5 + (pointsCount - (higherAmount + highestAmount)) * 2
+        elif unitsCount > 49:
+            highestAmount = unitsCount - 50
+            higherAmount = unitsCount - highestAmount - 20
+            calculatedAmount = higherAmount * 3 + highestAmount * 5 + (unitsCount - (higherAmount + highestAmount)) * 2
             self.billAmount = calculatedAmount
 
         # if 50 < pointsCount:
@@ -32,16 +32,7 @@ class ElectricityBillCalculator:
         #     self.billAmount = pointsCount * 2
 
 
-bill1 = ElectricityBillCalculator(49)
-print(bill1.billAmount)
+unitsCount = int(input("Enter number of units to calculate the electricity bill"))
 
-bill2 = ElectricityBillCalculator(52)
-print(bill2.billAmount)
-# bill1 = ElectricityBillCalculator(20)
-# print(bill1.billAmount)
-# bill1 = ElectricityBillCalculator(19)
-# print(bill1.billAmount)
-# bill1 = ElectricityBillCalculator(40)
-# print(bill1.billAmount)
-# bill1 = ElectricityBillCalculator(70)
-# print(bill1.billAmount)
+bill1 = ElectricityBillCalculator(unitsCount)
+print("Calculated bill amount for the entered unit count is ", bill1.billAmount)
