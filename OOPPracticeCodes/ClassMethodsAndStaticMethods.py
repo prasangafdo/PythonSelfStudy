34da8a0e859ba6fee1bca78f2f46235003069be6
Prasanga Fernando <prasangafdz@outlook.com>
2021-12-17 16:03:20 +0530

    Update ClassMethodsAndStaticMethods.py

---
+++ OOPPracticeCodes/ClassMethodsAndStaticMethods.py
@@ -0,0 +1,27 @@
+class employee:
+    increment = 1.5;
+
+    def __init__(self, firstname, lastname, salary):
+        self.fullName = firstname + " " + lastname
+        self.salary = salary
+
+    # These are instance variables. (Unique to every instance)
+    # These are started with self
+
+    def fullName(self):
+        # self.fullName = firstname + " " + lastname
+        return "self.fullName"
+
+    def calculateIncrement(self):
+        self.increment = self.increment * 1.5
+
+    def raiseAmount(self):
+        self.salary = int(self.salary * self.increment) # Casting to return as an integer
+
+
+employee1 = employee("Kasun", "Nirmala", 20000)
+print(employee1 .salary)
+employee1.raiseAmount()
+print(employee1.salary)
+
+
